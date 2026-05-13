c1fdb7c17db38e60b298c3487a45a3afac63b974
Gutema <guutamaa2011@gmail.com>
2026-05-13 11:49:01 +0300

    sidebar added

--- resources/views/boot.blade.php
+++ resources/views/boot.blade.php
@@ -334,6 +334,9 @@
                 </div>
                 </div>
                 <div>
+                    test sidebar
+                </div>
+                <div>
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3609.5741645301714!2d38.71541254441662!3d7.918675969982824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x17b49c6011ac0c85%3A0xbe2874ddcd7fa6f7!2sZiway%2C%20Ethiopia!5e1!3m2!1sen!2suk!4v1778659238054!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
